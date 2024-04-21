a6322b1f0403cedd1eae2cd6cd49a587c8be7d0b
MOHD BELAL IRSHAD <101103105+Belalirshad@users.noreply.github.com>
2024-04-21 11:42:12 +0530

    Add files via upload

---
+++ src/config/allowedOrigins.js
@@ -0,0 +1,9 @@
+// to allow the below definded origins to access the server
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://flixbackend.onrender.com",
+  "https://ab-flix-frontend.onrender.com",
+];
+
+module.exports = allowedOrigins;
